d99335ceca06b6b130d2b5a30adaed1650b147a9
Ajay Verma <ajayver.com@gmail.com>
2023-02-08 20:51:40 +0800

    Update sample-trigger.php

--- triggers/sample-trigger.php
+++ triggers/sample-trigger.php
@@ -12,20 +12,35 @@ class Automator_Sample_Trigger extends Uncanny_Automator\Recipe\Trigger {
 
 		$this->set_integration( 'AUTOMATOR_SAMPLE' );
 		$this->set_trigger_code( 'POST_CREATED_SAMPLE' );
-		$this->set_trigger_meta( 'POST_TYPES_DROPDOWN' );
+		$this->set_trigger_meta( 'POST_TYPE' );
 		/* Translators: Some information for translators */
-		$this->set_sentence( sprintf( esc_attr__( 'A {{a post type:%1$s}} is created sample trigger', 'automator-sample' ), 'POST_TYPES_DROPDOWN' ) );
+		$this->set_sentence( sprintf( esc_attr__( 'A {{a post type:%1$s}} is created sample trigger', 'automator-sample' ), 'POST_TYPE' ) );
 		/* Translators: Some information for translators */
 		$this->set_readable_sentence( esc_attr__( 'A {{a post type}} is created sample trigger', 'automator-sample' ) );
 
 		$this->add_action( 'wp_after_insert_post', 90, 4 );
+
+		$this->set_tokens(
+			array(
+				array(
+					'tokenId'         => 'POST_TITLE',
+					'tokenName'       => __( 'Post Title', 'automator-sample' ),
+					'tokenType'       => 'text',
+				),
+				array(
+					'tokenId'         => 'POST_URL',
+					'tokenName'       => __( 'Post URL', 'automator-sample' ),
+					'tokenType'       => 'text',
+				),
+			)
+		);
 	}
 
 	public function load_options() {
 
-		$post_types_dropdown = array(
+		$POST_TYPE = array(
 			'input_type'      => 'select',
-			'option_code'     => 'POST_TYPES_DROPDOWN',
+			'option_code'     => 'POST_TYPE',
 			'label'           => __( 'Post type', 'automator-sample' ),
 			'required'        => true,
 			'options'         => $this->helpers->get_post_types(),
@@ -34,9 +49,22 @@ class Automator_Sample_Trigger extends Uncanny_Automator\Recipe\Trigger {
 
 		return array(
 			'options' => array(
-				$post_types_dropdown
+				$POST_TYPE
 			)
 		);
+	}
+
+	/**
+	 * maybe_add_recipe_specific_tokens
+	 * 
+	 * Alter this method if you want to add recipe-specific tokens that depend on values selected in the trigger options.
+	 *
+	 * @param  mixed $tokens
+	 * @param  mixed $args
+	 * @return void
+	 */
+	public function maybe_add_recipe_specific_tokens( $tokens, $args ) {
+		return $tokens;
 	}
 
 	/**
@@ -45,13 +73,13 @@ class Automator_Sample_Trigger extends Uncanny_Automator\Recipe\Trigger {
 	public function validate_trigger( $trigger, $hook_args ) {
 
 		// Make sure the trigger has some value selected in the options
-		if ( ! isset( $trigger['meta']['POST_TYPES_DROPDOWN'] ) ) {
+		if ( ! isset( $trigger['meta']['POST_TYPE'] ) ) {
 			//Something is wrong, the trigger doesn't have the required option value.
 			return false;
 		}
 
 		// Get the dropdown value
-		$selected_post_type = $trigger['meta']['POST_TYPES_DROPDOWN'];
+		$selected_post_type = $trigger['meta']['POST_TYPE'];
 
 		// Parse the args from the wp_after_insert_post hook
 		$post_id = array_shift( $hook_args );
@@ -73,10 +101,19 @@ class Automator_Sample_Trigger extends Uncanny_Automator\Recipe\Trigger {
 		return true;
 	}
 
-	public function process_tokens( $completed_trigger, $hook_args ) {
 
-		elog( 'process_tokens' );
-		elog( $completed_trigger, '$completed_trigger' );
-		elog( $hook_args, '$hook_args' );
+	public function hydrate_tokens( $trigger, $hook_args ) {
+
+		$post_id = array_shift( $hook_args );
+		$post = array_shift( $hook_args );
+
+		$token_values = array(
+			'POST_TYPE' => $trigger['meta']['POST_TYPE'],
+			'POST_TITLE' => $post->post_title,
+			'POST_URL' => get_permalink( $post->ID )
+		); 
+
+		return $token_values;
 	}
+
 }
