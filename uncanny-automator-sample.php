27c81c0af7cd037a2a6991a428291a6f8b3665a2
Ajay Verma <ajayver.com@gmail.com>
2023-03-08 23:00:30 +0800

    Update uncanny-automator-sample.php

--- uncanny-automator-sample.php
+++ uncanny-automator-sample.php
@@ -22,24 +22,22 @@ function sample_integration_load_files() {
 	}
 
 	require_once 'helpers/helpers.php';
-	$helpers = new Helpers();
-
 	require_once 'sample-integration.php';
-	new Add_Sample_Integration( $helpers );
-
 	require_once 'settings/settings-sample.php';
-	new Sample_Integration_Settings( $helpers );
-
 	require_once 'actions/send-email-sample.php';
-	new Send_Email_Sample( $helpers );
-
 	require_once 'triggers/post-created-sample.php';
-	new Post_Created_Sample_Trigger( $helpers );
-
 	require_once 'triggers/anon-comment-submitted.php';
+
+	
+	$helpers = new Helpers();
+	
+	new Add_Sample_Integration( $helpers );
+	new Sample_Integration_Settings( $helpers );
+	new Send_Email_Sample( $helpers );
+	new Post_Created_Sample_Trigger( $helpers );
 	new Comment_Submitted_Sample( $helpers );
 
-	// Register an ajax endpoint for the next field
+	// Register an ajax endpoint for the get posts field
 	add_action( 'wp_ajax_automator_sample_get_posts', array( $helpers, 'ajax_get_posts' ) );
 
 }
