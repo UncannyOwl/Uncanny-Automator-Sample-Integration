fa3b2ff2959dd35af49bad11be597307065b4e3c
Ajay Verma <ajayver.com@gmail.com>
2023-03-29 20:53:22 +0800

    Update sample-integration.php

--- sample-integration.php
+++ sample-integration.php
@@ -11,9 +11,6 @@ class Sample_Integration extends \Uncanny_Automator\Integration {
 	protected function setup() {
 		$this->set_integration( 'SAMPLE_INTEGRATION' );
 		$this->set_name( 'Automator Sample Integration' );
-		$this->set_icon( 'sample-icon.svg' );
-		$this->set_icon_path( __DIR__ . '/img/' );
-		$this->set_plugin_file_path( dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'uncanny-automator-sample.php' );
-		$this->set_external_integration( true );
+		$this->set_icon_url( plugin_dir_url( __FILE__ ) . '/img/sample-icon.svg' );
 	}
 }
