e38ff31c3f905a515496608aea4c1be4e10a2954
Ajay Verma <ajayver.com@gmail.com>
2023-03-29 20:53:21 +0800

    Update uncanny-automator-sample.php

--- uncanny-automator-sample.php
+++ uncanny-automator-sample.php
@@ -1,19 +1,10 @@
-<?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
+<?php
 
 /**
- * @wordpress-plugin
  * Plugin Name:       Uncanny Automator Sample Integration
- * Plugin URI:        https://www.automatorplugin.com
- * Description:       Sample integration plugin for Uncanny Automator
- * Version:           1.0.0
- * Author:            Uncanny Automator
- * Author URI:        https://www.automatorplugin.com
- * License:           GPL-3.0+
- * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
- * Text Domain:       automator-sample
- * Domain Path:       /languages
  */
 
+add_action( 'automator_add_integration', 'sample_integration_load_files' );
 
 function sample_integration_load_files() {
 	// If this class doesn't exist Uncanny Automator plugin needs to be updated.
@@ -22,21 +13,23 @@ function sample_integration_load_files() {
 	}
 
 	require_once 'helpers/helpers.php';
-	require_once 'sample-integration.php';
-	require_once 'settings/settings-sample.php';
-	require_once 'actions/send-email-sample.php';
-	require_once 'triggers/post-created-sample.php';
-	require_once 'triggers/anon-comment-submitted.php';
-
-	
 	$helpers = new Helpers();
-	
-	new Sample_Integration( $helpers );
-	new Sample_Integration_Settings( $helpers );
-	new Send_Email_Sample( $helpers );
-	new Post_Created_Sample_Trigger( $helpers );
-	new Comment_Submitted_Sample( $helpers );
 
+	require_once 'sample-integration.php';
+	new Sample_Integration( $helpers );
+
+	require_once 'settings/settings-sample.php';
+	new Sample_Integration_Settings( $helpers );
+
+	require_once 'actions/send-email-sample.php';
+	new Send_Email_Sample( $helpers );
+
+	require_once 'triggers/post-created-sample.php';
+	new Post_Created_Sample_Trigger( $helpers );
+
+	require_once 'triggers/anon-comment-submitted.php';
+	new Comment_Submitted_Sample( $helpers );
+	
 	// Register an ajax endpoint for the get posts field
 	add_action( 'wp_ajax_automator_sample_get_posts', array( $helpers, 'ajax_get_posts' ) );
 
