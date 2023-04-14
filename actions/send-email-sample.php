dc9536ed62012d5ef05420b801462e2ee0deffe5
Ajay Verma <ajayver.com@gmail.com>
2023-04-14 21:03:31 +0800

    Update send-email-sample.php

--- actions/send-email-sample.php
+++ actions/send-email-sample.php
@@ -10,21 +10,25 @@ class Send_Email_Sample extends \Uncanny_Automator\Recipe\Action {
 	 */
 	protected function setup_action() {
 
+		// Define the Actions's info
 		$this->set_integration( 'SAMPLE_INTEGRATION' );
 		$this->set_action_code( 'SEND_EMAIL_SAMPLE' );
 		$this->set_action_meta( 'EMAIL_TO' );
 
-		/* translators: Action - WordPress */
+		// Define the Action's sentence
 		$this->set_sentence( sprintf( esc_attr__( 'Send an email to {{email address:%1$s}} from Sample Integration', 'automator-sample' ), $this->get_action_meta() ) );
-		/* translators: Action - WordPress */
 		$this->set_readable_sentence( esc_attr__( 'Send an {{email}} from Sample Integration', 'automator-sample' ) );
 		
 	}
-
+	
+	/**
+	 * Define the Action's options
+	 *
+	 * @return void
+	 */
 	public function options() {
 
 		return array(
-			/* translators: Email field */
 			Automator()->helpers->recipe->field->text(
 				array(
 					'option_code' => 'EMAIL_FROM',
@@ -35,7 +39,6 @@ class Send_Email_Sample extends \Uncanny_Automator\Recipe\Action {
 					'default'     => 'john@doe.com',
 				)
 			),
-			/* translators: Email field */
 			Automator()->helpers->recipe->field->text(
 				array(
 					'option_code' => 'EMAIL_TO',
@@ -43,25 +46,6 @@ class Send_Email_Sample extends \Uncanny_Automator\Recipe\Action {
 					'input_type'  => 'email',
 				)
 			),
-			/* translators: Email field */
-			Automator()->helpers->recipe->field->text(
-				array(
-					'option_code' => 'EMAIL_CC',
-					'label'       => 'CC',
-					'input_type'  => 'email',
-					'required'    => false,
-				)
-			),
-			/* translators: Email field */
-			Automator()->helpers->recipe->field->text(
-				array(
-					'option_code' => 'EMAIL_BCC',
-					'label'       => 'BCC',
-					'input_type'  => 'email',
-					'required'    => false,
-				)
-			),
-			/* translators: Email field */
 			Automator()->helpers->recipe->field->text(
 				array(
 					'option_code' => 'EMAIL_SUBJECT',
@@ -69,7 +53,6 @@ class Send_Email_Sample extends \Uncanny_Automator\Recipe\Action {
 					'input_type'  => 'text',
 				)
 			),
-			/* translators: Email field */
 			Automator()->helpers->recipe->field->text(
 				array(
 					'option_code' => 'EMAIL_BODY',
@@ -91,24 +74,29 @@ class Send_Email_Sample extends \Uncanny_Automator\Recipe\Action {
 
 		$action_meta = $action_data['meta'];
 
+		// Get the field values
 		$to = sanitize_email( Automator()->parse->text( $action_meta['EMAIL_TO'], $recipe_id, $user_id, $args ) );
 		$from = sanitize_email( Automator()->parse->text( $action_meta['EMAIL_FROM'], $recipe_id, $user_id, $args ) );
 		$subject = sanitize_text_field( Automator()->parse->text( $action_meta['EMAIL_SUBJECT'], $recipe_id, $user_id, $args ) );
 		$body = wp_filter_post_kses( stripslashes( ( Automator()->parse->text( $action_meta['EMAIL_BODY'], $recipe_id, $user_id, $args ) ) ) );
+		
+		//Set email headers
 		$headers = array( 
 			'Content-Type: text/html; charset=utf-8',
 			'From: ' . get_bloginfo('name') . ' <' . $from . '>',
 			'Reply-To: ' . get_bloginfo('name') . ' <' . $from . '>',
 		 );
 
+		// Send
 		$success = wp_mail( $to, $subject, $body, $headers );
 
+		// Handle errors
 		if ( ! $success ) {
 			$this->add_log_error( __( 'Email was not sent', 'automator-sample' ) );
 			return false;
 		}
 
+		// Always return true if everything was okay
 		return true;
-
 	}
 }
