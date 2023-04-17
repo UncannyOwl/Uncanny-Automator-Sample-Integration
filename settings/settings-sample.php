bdf9f05614be71e852c589362f15555eecf5abd7
Ajay Verma <ajayver.com@gmail.com>
2023-04-17 20:26:07 +0800

    Update settings-sample.php

--- settings/settings-sample.php
+++ settings/settings-sample.php
@@ -30,38 +30,32 @@ class Sample_Integration_Settings extends \Uncanny_Automator\Settings\Premium_In
 
 	public function get_status() {
 
+		// Not connected by default
 		$this->is_connected = false;
 
+		// Get the API key
 		$this->api_key = get_option( 'sample_api_key', false );
 		
+		// If there is an API key, we are connected
 		if ( false !== $this->api_key ) {
+
+			// Store the connected status for later use
 			$this->is_connected = true;
+
+			// Return this string to show the green checkmark
+			return 'success';
 		}
 
-		if ( false === $this->api_key ) {
-			return '';
-		}
-
-		return 'success';
+		// Return an empty string is not connected
+		return '';
 	}
 
 	/**
 	 * Creates the output of the settings page
 	 */
 	public function output_panel_content() {
-		// Get user data
-		$user_data = (object) array(
-			'username' => 'johndoe',
-			'name' => 'John Doe',
-			'email_address' => 'johndoe@automatorplugin.com'
-		);
 
-		// oAuth URL
-		$oauth_urls = (object) array(
-			'connect' => 'https://...',
-			'disconnect' => 'https://...',
-		);
-
+		// If the integration is not connected, output the field
 		if ( ! $this->is_connected ) {
 
 			$args = array(
@@ -73,7 +67,8 @@ class Sample_Integration_Settings extends \Uncanny_Automator\Settings\Premium_In
 
 			$this->text_input( $args );
 
-		} else { ?>
+		} else { // If the integration is connected, output the success message
+			?> 
 			<p> <?php echo __( 'You have successfully connected!', 'automator-sample' ); ?> </p>
 			<?php
 		}
@@ -82,12 +77,14 @@ class Sample_Integration_Settings extends \Uncanny_Automator\Settings\Premium_In
 
 	public function output_panel_bottom_right() {
 
+		// If we are connected, output the Save button
 		if ( ! $this->is_connected ) {
 			$button_label = __( 'Save settings', 'automator-sample' );
 
 			$this->submit_button( $button_label );
 		} else {
 
+			// Otherwise, show a button that will redirect with the disconnect flag in the URL
 			$button_label = __( 'Disconnect', 'automator-sample' );
 			$link = $this->get_settings_page_url() . '&disconnect=1';
 
@@ -97,9 +94,13 @@ class Sample_Integration_Settings extends \Uncanny_Automator\Settings\Premium_In
 
 	public function settings_updated() {
 
+		// Get the setting
 		$this->api_key = get_option( 'sample_api_key', false );
 
+		// Run any validation and add alerts
 		if ( is_numeric( $this->api_key ) ) {
+
+			// Display a success message
 			$this->add_alert(
 				array(
 					'type'    => 'success',
@@ -108,6 +109,10 @@ class Sample_Integration_Settings extends \Uncanny_Automator\Settings\Premium_In
 				)
 			);
 		} else {
+			// Delete the invalid APi key
+			delete_option( 'sample_api_key' );
+
+			// Display an error
 			$this->add_alert(
 				array(
 					'type'    => 'error',
@@ -116,21 +121,25 @@ class Sample_Integration_Settings extends \Uncanny_Automator\Settings\Premium_In
 				)
 			);
 		}
-
 	}
 
 	public function disconnect() {
 
+		// Make sure this settings page is the one that is active
 		if ( ! $this->is_current_page_settings() ) {
 			return;
 		}
 
+		// Check that the URL has our custom disconnect flag
 		if ( '1' !== automator_filter_input( 'disconnect' ) ) {
 			return;
 		}
 
+		// Delete the API key
 		delete_option( 'sample_api_key' );
 
+
+		// Redirect back to the settings page
 		wp_safe_redirect( $this->get_settings_page_url() );
 
 		exit;
